562334529c17b2dd352b8b7d3ef4c39fd75c0db9
e404nichtgefunden <thecadboyz@gmail.com>
2025-04-27 16:36:50 +0700

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -2,11 +2,11 @@
 
 echo "[*] Membuat direktori /root/telegram-bot"
 sudo chmod 777 /root
-sudo mkdir -p /root/telegram-bot
+sudo mkdir -p /root/tele-bot
 
 echo "[*] Memindahkan file..."
 cd /workspace/code
-sudo cp deploy3.py telegram-bot.service commands.sh /root/telegram-bot
+sudo cp deploy3.py tele-bot.service commands.sh /root/tele-bot
 
 echo "[*] Pindah ke folder bot..."
 cd /root/telegram-bot || exit
@@ -17,7 +17,7 @@ sudo rm -rf /usr/local/lib/python3.*/dist-packages/blinker*
 sudo rm -rf /usr/lib/python3.*/dist-packages/blinker* 
 pip3 install --upgrade setuptools pip 
 pip3 install --no-cache-dir flask python-telegram-bot==20.8
-pip3 install --no-cache-dir telegram
+pip3 install --no-cache-dir telegram psutil
 pip3 install python-telegram-bot --upgrade
 sudo chmod 777 /etc
 sudo chmod 777 /etc/systemd
@@ -27,14 +27,14 @@ echo "[*] Menyalin systemd service..."
 sudo cp telegram-bot.service /etc/systemd/system
 
 echo "[*] Menyesuaikan path di service file..."
-sudo sed -i "s|/root/stx.py|/root/telegram-bot|g" /etc/systemd/system/telegram-bot.service
-sudo sed -i "s|/root/stx.py|/root/telegram-bot/stx.py|g" /etc/systemd/system/telegram-bot.service
+sudo sed -i "s|/root/stx.py|/root/tele-bot|g" /etc/systemd/system/tele-bot.service
+sudo sed -i "s|/root/stx.py|/root/tele-bot/stx.py|g" /etc/systemd/system/tele-bot.service
 
 echo "[*] Reload systemd..."
 sudo systemctl daemon-reload
 
 echo "[*] Enable dan start bot..."
-sudo systemctl enable telegram-bot
-sudo systemctl start telegram-bot
+sudo systemctl enable tele-bot
+sudo systemctl start tele-bot
 
 echo "[✓] Bot berhasil diinstall, systemd aktif!"
